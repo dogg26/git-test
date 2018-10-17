b6d45dcc277f8b0acdd5456ec90aa3dc9793aa72
dogg <31368621@qq.com>
2018-10-17 08:46:30 +0800

    test1

--- git-test/src/com/itcast/git/Demo1.java
+++ git-test/src/com/itcast/git/Demo1.java
@@ -4,4 +4,9 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
+
+    public static void test(){
+        System.out.println("hello git");
+    }
 }
